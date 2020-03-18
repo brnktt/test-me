40edcbb4f4ccbdf19ebe9701cf8e973f424ca352
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-18 02:01:31 +0100

    Added better output styling + excluded forbidden directories like node_modules

--- runner.js
+++ runner.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const path = require("path");
+const chalk = require("chalk");
+
+const forbiddenDirs = ["node_modules"];
 class Runner {
   constructor() {
     this.testFiles = [];
@@ -7,6 +10,8 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
+      console.log(chalk.gray(`---- ${file.shortName}`));
+
       const beforeEaches = [];
       global.beforeEach = fn => {
         beforeEaches.push(fn);
@@ -15,18 +20,18 @@ class Runner {
         beforeEaches.forEach(func => func());
         try {
           fn();
-          console.log(`OK - ${desc}`);
+          console.log(chalk.green(`\tOK - ${desc}`));
         } catch (err) {
-          console.log(`X - ${desc}`);
-          console.log("\t", err.message);
+          const message = err.message.replace(/\n/g, "\n\t\t");
+          console.log(chalk.red(`\tX - ${desc}`));
+          console.log(chalk.red("\t", message));
         }
       };
 
       try {
         require(file.name);
-        console.log(`OK - ${file.name}`);
       } catch (err) {
-        console.log(err);
+        console.log(chalk.red(err));
       }
     }
   }
@@ -39,8 +44,8 @@ class Runner {
       const stats = await fs.promises.lstat(filepath);
 
       if (stats.isFile() && file.includes(".test.js")) {
-        this.testFiles.push({ name: filepath });
-      } else if (stats.isDirectory()) {
+        this.testFiles.push({ name: filepath, shortName: file });
+      } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
         const childFiles = await fs.promises.readdir(filepath);
 
         files.push(...childFiles.map(f => path.join(file, f)));
--- sampleproject/test/forEach.test.js
+++ sampleproject/test/forEach.test.js
@@ -22,3 +22,5 @@ it("Should sum an array", () => {
 it("beforeEach is ran each time", () => {
   assert.strictEqual(numbers.length, 3);
 });
+
+// {{)}}
